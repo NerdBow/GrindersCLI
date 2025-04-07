570df1dbcb2ec080c9fbff2a22ef02bc6a122190
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-04-07 14:18:35 -0400

    Add CreateLogModelInit func & consts for fields and msgs

--- internal/model/createLog.go
+++ internal/model/createLog.go
@@ -3,14 +3,54 @@ package model
 import (
 	"github.com/NerdBow/GrindersTUI/internal/keymap"
 	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+type CreateLogModelSwitch uint8
+
+type EmptyFieldErrorMsg uint8
+
+const (
+	NameField     uint8 = 0
+	CategoryField       = 1
+	GoalField           = 2
+	ConfirmButton       = iota
+
+	HomeSwitch CreateLogModelSwitch = iota
+	TimerSwitch
+
+	UsernameFieldEmpty EmptyFieldErrorMsg = iota
+	CategoryFieldEmpty
+	GoalFieldEmpty
+)
+
 type CreateLogModel struct {
+	focusIndex int
+	inputs     []textinput.Model
 }
 
 func CreateLogModelInit() *CreateLogModel {
-	return &CreateLogModel{}
+	m := CreateLogModel{
+		inputs: make([]textinput.Model, 3),
+	}
+	for i := range 3 {
+		t := textinput.New()
+		t.CharLimit = 100
+		t.PromptStyle = textInputFocusedStyle
+		t.TextStyle = textInputFocusedStyle
+		switch uint8(i) {
+		case NameField:
+			t.Placeholder = "Name"
+			t.Focus()
+		case CategoryField:
+			t.Placeholder = "Category"
+		case GoalField:
+			t.Placeholder = "Goal"
+		}
+		m.inputs[i] = t
+	}
+	return &m
 }
 
 func (m *CreateLogModel) Init() tea.Cmd {
