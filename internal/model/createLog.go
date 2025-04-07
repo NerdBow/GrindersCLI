6c611688fdc7ab550193409790bb610a71c3fa50
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-04-07 14:43:41 -0400

    Add forgotten strings lib for the View method of CreateLogModel

--- internal/model/createLog.go
+++ internal/model/createLog.go
@@ -5,6 +5,7 @@ import (
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"strings"
 )
 
 type CreateLogModelSwitch uint8
