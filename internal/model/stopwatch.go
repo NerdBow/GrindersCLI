0aeca3ac01fbbe60e9013e3985b0d4a1d8a6976c
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-04-08 12:17:51 -0400

    Add focusIndex field to StopwatchModel & add consts for fields & add logic for View func

--- internal/model/stopwatch.go
+++ internal/model/stopwatch.go
@@ -6,10 +6,17 @@ import (
 	"github.com/charmbracelet/bubbles/stopwatch"
 	tea "github.com/charmbracelet/bubbletea"
 	"strings"
+
+const (
+	TimerField = iota
+	RestField
+	FinishLogField
 )
 
 type StopwatchModel struct {
-	sw stopwatch.Model
+	sw         stopwatch.Model
+	focusIndex int
+}
 }
 
 func (m *StopwatchModel) Init() tea.Cmd {
@@ -21,5 +28,31 @@ func (m *StopwatchModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m *StopwatchModel) View() string {
-	return ""
+	b := strings.Builder{}
+	durationTime := fmt.Sprintf("%02d:%02d:%02d", int(m.sw.Elapsed().Hours())%60, int(m.sw.Elapsed().Minutes())%60, int(m.sw.Elapsed().Seconds())%60)
+	renderedTime := textInputUnfocusedStyle.Render(durationTime)
+
+	if m.focusIndex == TimerField {
+		renderedTime = textInputFocusedStyle.Render(durationTime)
+	}
+	b.WriteString(renderedTime)
+
+	b.WriteRune('\n')
+
+	restText := textInputUnfocusedStyle.Render("Rest")
+
+	if m.focusIndex == RestField {
+		restText = textInputFocusedStyle.Render("Rest")
+	}
+
+	b.WriteString(restText)
+
+	b.WriteRune('\n')
+
+	confirmText := textInputUnfocusedStyle.Render("Finish Log")
+	if m.focusIndex == FinishLogField {
+		confirmText = textInputFocusedStyle.Render("Finish Log")
+	}
+	b.WriteString(confirmText)
+	return b.String()
 }
