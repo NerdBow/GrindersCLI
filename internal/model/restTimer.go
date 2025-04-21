6d36118231a95de8ff3dbda2720654b6a0ff75c3
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-04-21 12:26:31 -0400

    Add restTimer.go with base struct & imports

---
+++ internal/model/restTimer.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"time"
+
+	"github.com/NerdBow/GrindersTUI/internal/keymap"
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type RestTimerModel struct {
+	duration time.Duration
+}
