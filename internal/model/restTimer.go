34ba546ae249e5c019bf4fac5894a4b58ccc4e1d
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-04-24 11:07:52 -0400

    Add timer logic & keymapping to Update func of RestTimerModel

--- internal/model/restTimer.go
+++ internal/model/restTimer.go
@@ -5,10 +5,12 @@ import (
 	"strings"
 	"time"
 
-	// "github.com/NerdBow/GrindersTUI/internal/keymap"
-	// "github.com/charmbracelet/bubbles/key"
+	"github.com/NerdBow/GrindersTUI/internal/keymap"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 )
+
+type tickMsg struct{}
 
 type RestTimerModel struct {
 	done     bool
@@ -31,7 +33,37 @@ func (m *RestTimerModel) Init() tea.Cmd {
 }
 
 func (m *RestTimerModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return nil, nil
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, keymap.VimBinding.Exit):
+			return m, func() tea.Msg { return ModelMsg{CurrentModel: RestTimer, NextModel: Stopwatch, Other: nil} }
+		case key.Matches(msg, keymap.VimBinding.Select):
+			if !m.running {
+				if m.done {
+					return m, func() tea.Msg { return ModelMsg{CurrentModel: RestTimer, NextModel: Stopwatch, Other: nil} }
+				}
+				m.running = true
+				return m, tea.Tick(time.Second, func(_ time.Time) tea.Msg {
+					return tickMsg{}
+				})
+			}
+		}
+	case tickMsg:
+		if !m.running {
+			return m, nil
+		}
+		if m.duration <= time.Duration(0) {
+			m.running = false
+			m.done = true
+			m.text = "Your break is done! Press select to get back to the stopwatch!"
+		}
+		m.duration -= time.Second
+		return m, tea.Tick(time.Second, func(_ time.Time) tea.Msg {
+			return tickMsg{}
+		})
+	}
+	return m, nil
 }
 
 func (m *RestTimerModel) View() string {
