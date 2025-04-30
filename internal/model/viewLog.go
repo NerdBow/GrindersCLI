a7fa572d37fce4380baf409aa4c5fa47e6031614
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-04-30 08:45:46 -0400

    Add logic for selecting choices in View & Update method of ViewLogModel

--- internal/model/viewLog.go
+++ internal/model/viewLog.go
@@ -30,10 +30,36 @@ func (m *ViewLogModel) Init() tea.Cmd {
 }
 
 func (m *ViewLogModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, keymap.VimBinding.Up):
+			m.focusIndex = ((m.focusIndex-1)%len(m.choices) + len(m.choices)) % len(m.choices)
+			return m, nil
+		case key.Matches(msg, keymap.VimBinding.Down):
+			m.focusIndex = (m.focusIndex + 1) % len(m.choices)
+			return m, nil
+		case key.Matches(msg, keymap.VimBinding.Exit):
+			return m, func() tea.Msg { return ModelMsg{ViewLog, Home, nil} }
+		}
+
+	}
 	return m, nil
 }
 
 func (m *ViewLogModel) View() string {
-	b := strings.Builder{}
+	var b strings.Builder
+
+	for i := range m.choices {
+		if i == m.focusIndex {
+			b.WriteString("> ")
+		}
+
+		b.WriteString(m.choices[i])
+
+		if i < len(m.choices)-1 {
+			b.WriteRune('\n')
+		}
+	}
 	return b.String()
 }
