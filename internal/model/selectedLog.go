715beecdc552eca57ab2874f79b88f8f5fe5ae64
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-05-10 20:47:15 -0400

    Update View method of SelectedLogModel to display all log information

--- internal/model/selectedLog.go
+++ internal/model/selectedLog.go
@@ -1,10 +1,12 @@
 package model
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/NerdBow/GrindersTUI/internal/keymap"
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
-	"strings"
 )
 
 type SelectedLogModel struct {
@@ -25,5 +27,17 @@ func (m *SelectedLogModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m *SelectedLogModel) View() string {
 	b := strings.Builder{}
+	b.WriteString(fmt.Sprintf("Id: %d", m.log.Id))
+	b.WriteRune('\n')
+	b.WriteString(fmt.Sprintf("Date: %s", m.log.DateString()))
+	b.WriteRune('\n')
+	b.WriteString(fmt.Sprintf("Name: %s", m.log.Name))
+	b.WriteRune('\n')
+	b.WriteString(fmt.Sprintf("Category: %s", m.log.Category))
+	b.WriteRune('\n')
+	b.WriteString(fmt.Sprintf("Goal: %s", m.log.Goal))
+	b.WriteRune('\n')
+	b.WriteString(fmt.Sprintf("Duration: %s", m.log.DurationString()))
+	b.WriteRune('\n')
 	return b.String()
 }
